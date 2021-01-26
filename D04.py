a1180058e5534f8876d4d2dbc1377e7e12103022
Eddie Tseng <wayne761217@hotmail.com>
2021-01-26 23:58:40 +0800

    feat: D04作業修正

--- D04.py
+++ D04.py
@@ -5,8 +5,8 @@ math_score = np.array([60, 85, 60, 68, 55, 60])
 chinese_score = np.array([65, 90, 82, 72, 66, 77])
 # 1.有多少學生英文成績比數學成績高?
 # ANS: 3
-print(np.count_nonzero(np.greater(english_score, math_score)))
+print(np.sum(np.greater(english_score, math_score)))
 
 # 2.是否全班同學最高分都是國文?
 # ANS: yes
-print(np.logical_and(np.greater(chinese_score, math_score), np.greater(chinese_score, english_score)))
+print(np.logical_and(np.greater(chinese_score, math_score), np.greater(chinese_score, english_score)).any())
